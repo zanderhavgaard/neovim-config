adf97db2d1a8d78c0ab944c23e5078dfd91bd2d7
zanderhavgaard <zander@havgaard.dk>
2020-07-23 15:08:20 +0200

    updated update script

--- update.sh
+++ update.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo -e "Updating vim python integration ..."
-pip install --user --upgrade pynvim msgpack smdv
+pip install --user --upgrade pynvim neovim-remote msgpack smdv
+
+echo -e "Updating vim node integration ..."
+echo -e "Command to be run: sudo npm install -g neovim"
+sudo npm install -g neovim
 
 echo -e "Upgrading vim-plug ..."
 nvim +PlugUpgrade +qa
