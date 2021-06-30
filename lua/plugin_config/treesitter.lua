8be12041688e2b0b256cfe85f31144f6746336e6
zanderhavgaard <zander@havgaard.dk>
2021-06-30 20:20:16 +0200

    disabled tree-sitter indent module

--- lua/plugin_config/treesitter.lua
+++ lua/plugin_config/treesitter.lua
@@ -5,7 +5,7 @@ require("nvim-treesitter.configs").setup(
             enable = true
         },
         indent = {
-            enable = true
+            enable = false
         },
         incremental_selection = {
             enable = true
