184535a1f61071693c8f9c324d1edc54f5248425
zanderhavgaard <zander@havgaard.dk>
2021-12-12 01:17:10 +0100

    commented deprecated lsp option

--- lua/plugin_config/bufferline.lua
+++ lua/plugin_config/bufferline.lua
@@ -4,8 +4,9 @@ require("bufferline").setup {
         show_buffer_close_icons = false,
         show_close_icon = false,
         -- show lsp diagnostics in bufferline
+        -- TODO enable?
         -- TODO check that this works ...
-        diagnostics = "nvim_lsp",
+        -- diagnostics = "nvim_lsp",
         -- offset bufferline left of file browser
         offsets = {{filetype = "NvimTree", text = "", highlight = "Directory", text_align = "left", padding = 1}},
         -- show tab numbers on the right, also highlight buffers that are in the same tab
