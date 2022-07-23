7bb815c3f70c677918bc857699585740d0fb60fa
zanderhavgaard <zander@havgaard.dk>
2022-07-23 20:36:52 +0200

    add config block to increase packer git clone timeout

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,198 +5,206 @@ local use = packer.use
 
 -- setup packages to be managed by packer
 return packer.startup(
-    function()
-        -- Packer can manage itself
-        use "wbthomason/packer.nvim"
+    {
+        function()
+            -- Packer can manage itself
+            use "wbthomason/packer.nvim"
 
-        -- sensible default settings
-        use "tpope/vim-sensible"
+            -- sensible default settings
+            use "tpope/vim-sensible"
 
-        -- use icons and glyphs
-        use "kyazdani42/nvim-web-devicons"
+            -- use icons and glyphs
+            use "kyazdani42/nvim-web-devicons"
 
-        -- autoclose backets and other delimiters
-        use "Raimondi/delimitMate"
+            -- autoclose backets and other delimiters
+            use "Raimondi/delimitMate"
 
-        -- easily change delimiters
-        use "tpope/vim-surround"
+            -- easily change delimiters
+            use "tpope/vim-surround"
 
-        -- autocluse html/xml style tags
-        use "alvan/vim-closetag"
+            -- autocluse html/xml style tags
+            use "alvan/vim-closetag"
 
-        -- easily close all buffers not open in a window
-        use "artnez/vim-wipeout"
+            -- easily close all buffers not open in a window
+            use "artnez/vim-wipeout"
 
-        -- show git status in left editor gutter
-        -- use "airblade/vim-gitgutter"
-        use "lewis6991/gitsigns.nvim"
+            -- show git status in left editor gutter
+            -- use "airblade/vim-gitgutter"
+            use "lewis6991/gitsigns.nvim"
 
-        -- git integration
-        use "tpope/vim-fugitive"
+            -- git integration
+            use "tpope/vim-fugitive"
 
-        -- view git blame for lines in visual mode
-        use "APZelos/blamer.nvim"
+            -- view git blame for lines in visual mode
+            use "APZelos/blamer.nvim"
 
-        -- which-key implemented in lua
-        use {
-            "folke/which-key.nvim",
-            config = function()
-                require("plugin_config.which-key")
-            end
-        }
+            -- which-key implemented in lua
+            use {
+                "folke/which-key.nvim",
+                config = function()
+                    require("plugin_config.which-key")
+                end
+            }
 
-        -- statusline
-        use {
-            "glepnir/galaxyline.nvim",
-            branch = "main",
-            -- loac custom config
-            config = function()
-                require "plugin_config.galaxyline"
-            end,
-            -- some optional icons
-            requires = {
-                "kyazdani42/nvim-web-devicons",
-                opt = true
+            -- statusline
+            use {
+                "glepnir/galaxyline.nvim",
+                branch = "main",
+                -- loac custom config
+                config = function()
+                    require "plugin_config.galaxyline"
+                end,
+                -- some optional icons
+                requires = {
+                    "kyazdani42/nvim-web-devicons",
+                    opt = true
+                }
+            }
+
+            -- nicer tab/bufferline
+            use {
+                "akinsho/nvim-bufferline.lua",
+                requires = "kyazdani42/nvim-web-devicons"
+            }
+
+            -- tree file browser
+            use {
+                "kyazdani42/nvim-tree.lua",
+                requires = "kyazdani42/nvim-tree.lua",
+                config = function()
+                    require "plugin_config.nvim-tree"
+                end
+            }
+
+            -- automatically set indent width
+            use "tpope/vim-sleuth"
+
+            -- draw indent guides
+            use "lukas-reineke/indent-blankline.nvim"
+
+            -- smooth scrolling
+            use "psliwka/vim-smoothie"
+
+            -- startify splash screen
+            -- use "mhinz/vim-startify"
+
+            -- dashboard inspired by DOOM emacs
+            -- use "glepnir/dashboard-nvim"
+            use {
+                "glepnir/dashboard-nvim",
+                config = function()
+                    require "plugin_config.dashboard"
+                end
+            }
+
+            -- keymap cheatsheet
+            use "lifepillar/vim-cheat40"
+
+            -- swap buffers in windows using directions
+            use "caenrique/swap-buffers.nvim"
+
+            -- fuzzy finder and more
+            use {
+                "nvim-telescope/telescope.nvim",
+                requires = {
+                    {"nvim-lua/popup.nvim"},
+                    {"nvim-lua/plenary.nvim"}
+                }
+            }
+
+            -- preview markdown files in a browser window
+            use "suan/vim-instant-markdown"
+
+            -- show colorcodes as background on strings
+            use "chrisbra/Colorizer"
+
+            -- show relative line numbers on focues buffer, show absolute on all others
+            use "jeffkreeftmeijer/vim-numbertoggle"
+
+            -- file handling commands
+            use "tpope/vim-eunuch"
+
+            -- toggle line comments
+            use "scrooloose/nerdcommenter"
+
+            -- highlight unique characters when jumping on same line
+            use "unblevable/quick-scope"
+
+            -- autocompletion
+            -- use "Shougo/deoplete.nvim"
+            -- use python jedi with deoplete
+            -- use "deoplete-plugins/deoplete-jedi"
+            -- vim.cmd("let g:deoplete#enable_at_startup = 1")
+
+            -- completion engine and plugins
+            use {
+                "ms-jpq/coq_nvim",
+                branch = "coq"
+            }
+            -- snippets for coq
+            use {
+                "ms-jpq/coq.artifacts",
+                branch = "artifacts"
+            }
+            -- 3rd party integrations
+            -- TODO needs config
+            -- use {
+            -- "ms-jpq/coq.thirdparty",
+            -- branch = "3p"
+            -- }
+
+            -- asynchronous lint engine
+            use "dense-analysis/ale"
+
+            -- golang integration
+            -- use {
+            -- "fatih/vim-go",
+            -- run = ":GoUpdateBinaries"
+            -- }
+
+            -- better syntax highlighting and more
+            use {
+                "nvim-treesitter/nvim-treesitter",
+                run = ":TSUpdate"
+            }
+
+            -- color brackets by scope
+            use {
+                "p00f/nvim-ts-rainbow",
+                requires = {
+                    "nvim-treesitter/nvim-treesitter"
+                }
+            }
+
+            -- integrate native spellchecker with treesitter
+            use "lewis6991/spellsitter.nvim"
+
+            -- unofficial support for terraform in vim
+            use "hashivim/vim-terraform"
+
+            -- suggestions and tab completion for ':' commands
+            use "gelguy/wilder.nvim"
+
+            -- create ascii tables in vim
+            use "dhruvasagar/vim-table-mode"
+
+            -- ===== colorschemes =====
+
+            -- DSL for colorschemes
+            use "rktjmp/lush.nvim"
+
+            use "ful1e5/onedark.nvim"
+            -- use "navarasu/onedark.nvim"
+            use "gruvbox-community/gruvbox"
+            use "dracula/vim"
+            use "folke/tokyonight.nvim"
+            use({"catppuccin/nvim", as = "catppuccin"})
+        end,
+        config = {
+            git = {
+                -- avoid timeout on cloning large repos on a slow connection ...
+                clone_timeout = 600
             }
         }
-
-        -- nicer tab/bufferline
-        use {
-            "akinsho/nvim-bufferline.lua",
-            requires = "kyazdani42/nvim-web-devicons"
-        }
-
-        -- tree file browser
-        use {
-            "kyazdani42/nvim-tree.lua",
-            requires = "kyazdani42/nvim-tree.lua",
-            config = function()
-                require "plugin_config.nvim-tree"
-            end
-        }
-
-        -- automatically set indent width
-        use "tpope/vim-sleuth"
-
-        -- draw indent guides
-        use "lukas-reineke/indent-blankline.nvim"
-
-        -- smooth scrolling
-        use "psliwka/vim-smoothie"
-
-        -- startify splash screen
-        -- use "mhinz/vim-startify"
-
-        -- dashboard inspired by DOOM emacs
-        -- use "glepnir/dashboard-nvim"
-        use {
-            "glepnir/dashboard-nvim",
-            config = function()
-                require "plugin_config.dashboard"
-            end
-        }
-
-        -- keymap cheatsheet
-        use "lifepillar/vim-cheat40"
-
-        -- swap buffers in windows using directions
-        use "caenrique/swap-buffers.nvim"
-
-        -- fuzzy finder and more
-        use {
-            "nvim-telescope/telescope.nvim",
-            requires = {
-                {"nvim-lua/popup.nvim"},
-                {"nvim-lua/plenary.nvim"}
-            }
-        }
-
-        -- preview markdown files in a browser window
-        use "suan/vim-instant-markdown"
-
-        -- show colorcodes as background on strings
-        use "chrisbra/Colorizer"
-
-        -- show relative line numbers on focues buffer, show absolute on all others
-        use "jeffkreeftmeijer/vim-numbertoggle"
-
-        -- file handling commands
-        use "tpope/vim-eunuch"
-
-        -- toggle line comments
-        use "scrooloose/nerdcommenter"
-
-        -- highlight unique characters when jumping on same line
-        use "unblevable/quick-scope"
-
-        -- autocompletion
-        -- use "Shougo/deoplete.nvim"
-        -- use python jedi with deoplete
-        -- use "deoplete-plugins/deoplete-jedi"
-        -- vim.cmd("let g:deoplete#enable_at_startup = 1")
-
-        -- completion engine and plugins
-        use {
-            "ms-jpq/coq_nvim",
-            branch = "coq"
-        }
-        -- snippets for coq
-        use {
-            "ms-jpq/coq.artifacts",
-            branch = "artifacts"
-        }
-        -- 3rd party integrations
-        -- TODO needs config
-        -- use {
-        -- "ms-jpq/coq.thirdparty",
-        -- branch = "3p"
-        -- }
-
-        -- asynchronous lint engine
-        use "dense-analysis/ale"
-
-        -- golang integration
-        -- use {
-        -- "fatih/vim-go",
-        -- run = ":GoUpdateBinaries"
-        -- }
-
-        -- better syntax highlighting and more
-        use {
-            "nvim-treesitter/nvim-treesitter",
-            run = ":TSUpdate"
-        }
-
-        -- color brackets by scope
-        use {
-            "p00f/nvim-ts-rainbow",
-            requires = {
-                "nvim-treesitter/nvim-treesitter"
-            }
-        }
-
-        -- integrate native spellchecker with treesitter
-        use "lewis6991/spellsitter.nvim"
-
-        -- unofficial support for terraform in vim
-        use "hashivim/vim-terraform"
-
-        -- suggestions and tab completion for ':' commands
-        use "gelguy/wilder.nvim"
-
-        -- create ascii tables in vim
-        use "dhruvasagar/vim-table-mode"
-
-        -- ===== colorschemes =====
-
-        -- DSL for colorschemes
-        use "rktjmp/lush.nvim"
-
-        use "ful1e5/onedark.nvim"
-        -- use "navarasu/onedark.nvim"
-        use "gruvbox-community/gruvbox"
-        use "dracula/vim"
-        use "folke/tokyonight.nvim"
-        use({"catppuccin/nvim", as = "catppuccin"})
-    end
+    }
 )
