36b49f2934db207d2fa3ca835ae4c3b0ddec0b4d
zanderhavgaard <zander@havgaard.dk>
2022-08-16 15:42:20 +0200

    try to fix bufferline after switching to chadtree

--- lua/plugin_config/bufferline.lua
+++ lua/plugin_config/bufferline.lua
@@ -7,8 +7,9 @@ require("bufferline").setup {
         -- TODO enable?
         -- TODO check that this works ...
         -- diagnostics = "nvim_lsp",
+        -- TODO this seems to not be working with chadtree ...
         -- offset bufferline left of file browser
-        offsets = {{filetype = "NvimTree", text = "", highlight = "Directory", text_align = "left", padding = 1}},
+        offsets = {{filetype = "CHADtree", text = "", highlight = "Directory", text_align = "left", padding = 1}},
         -- show tab numbers on the right, also highlight buffers that are in the same tab
         show_tab_indicators = true,
         sort_by = "directory"
