16367627ce941ce02f2c30a33865ec48c8e1bf48
zanderhavgaard <zander@havgaard.dk>
2022-10-18 14:35:28 +0200

    update cmp config

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -46,9 +46,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp
-                                                                     .protocol
-                                                                     .make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- TODO fix
 -- disable virutal text
