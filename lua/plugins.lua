ceb19034aa1da9fc1ac7dc4a659b146f8e6b4fb1
zanderhavgaard <zander@havgaard.dk>
2022-10-30 22:33:40 +0100

    switch to neo-tree file browser

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -59,12 +59,37 @@ return packer.startup({
         }
 
         -- tree file browser
+        vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]]) -- Unless you are still migrating, remove the deprecated commands from v1.x
         use {
-            "kyazdani42/nvim-tree.lua",
+            "nvim-neo-tree/neo-tree.nvim",
+            branch = "v2.x",
             requires = {
-                "kyazdani42/nvim-web-devicons" -- optional, for file icons
-            },
-            tag = "nightly" -- optional, updated every week. (see issue #1193)
+                "nvim-lua/plenary.nvim", "kyazdani42/nvim-web-devicons",
+                "MunifTanjim/nui.nvim", {
+                    -- only needed if you want to use the commands with "_with_window_picker" suffix
+                    's1n7ax/nvim-window-picker',
+                    tag = "v1.*",
+                    config = function()
+                        require'window-picker'.setup({
+                            autoselect_one = true,
+                            include_current = false,
+                            filter_rules = {
+                                -- filter using buffer options
+                                bo = {
+                                    -- if the file type is one of following, the window will be ignored
+                                    filetype = {
+                                        'neo-tree', "neo-tree-popup", "notify"
+                                    },
+
+                                    -- if the buffer type is one of following, the window will be ignored
+                                    buftype = {'terminal', "quickfix"}
+                                }
+                            },
+                            other_win_hl_color = '#e35e4f'
+                        })
+                    end
+                }
+            }
         }
 
         -- automatically set indent width
