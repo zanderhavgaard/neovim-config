47188bb0d2f51c0c31749cb9ba24bf5093455c9a
zanderhavgaard <zander@havgaard.dk>
2022-11-02 14:04:45 +0100

    move window-picker config to seperate file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -70,23 +70,7 @@ return packer.startup({
                     's1n7ax/nvim-window-picker',
                     tag = "v1.*",
                     config = function()
-                        require'window-picker'.setup({
-                            autoselect_one = true,
-                            include_current = false,
-                            filter_rules = {
-                                -- filter using buffer options
-                                bo = {
-                                    -- if the file type is one of following, the window will be ignored
-                                    filetype = {
-                                        'neo-tree', "neo-tree-popup", "notify"
-                                    },
-
-                                    -- if the buffer type is one of following, the window will be ignored
-                                    buftype = {'terminal', "quickfix"}
-                                }
-                            },
-                            other_win_hl_color = '#e35e4f'
-                        })
+                        require "plugin_config.nvim-window-picker"
                     end
                 }
             }
