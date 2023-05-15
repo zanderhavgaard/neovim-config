309ec1339d6cd45df1c9269098db6f01262a7a42
zanderhavgaard <zander@havgaard.dk>
2023-05-15 14:25:58 +0200

    add mason to list of filetype excludes for indentline config

--- lua/plugin_config/indentline.lua
+++ lua/plugin_config/indentline.lua
@@ -2,7 +2,7 @@
 require("indent_blankline").setup({
 	char = "▏",
 	first_char = "▏",
-	filetype_exclude = { "help", "terminal", "startify", "dashboard", "packer", "alpha" },
+	filetype_exclude = { "help", "terminal", "startify", "dashboard", "packer", "alpha", "mason" },
 	buftype_exclude = { "terminal" },
 	show_trailing_blankline_indent = true,
 	show_first_indent_level = true,
