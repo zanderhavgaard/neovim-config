4467676a1fc4272478b534bc205ceaaa31ea49b5
zanderhavgaard <zander@havgaard.dk>
2023-10-15 00:38:53 +0200

    change indentblankline plugin config to default v3

--- lua/plugin_config/indentline.lua
+++ lua/plugin_config/indentline.lua
@@ -1,23 +1,4 @@
 local vim = vim
 
 -- settings for indent-blankline plugin
-require("indent_blankline").setup({
-	char = "▏",
-	first_char = "▏",
-	filetype_exclude = {
-		"help",
-		"terminal",
-		"dashboard",
-		"alpha",
-		"mason",
-		"lspinfo",
-		"TelescopePrompt",
-		"TelescopeResults",
-	},
-	buftype_exclude = { "terminal" },
-	show_trailing_blankline_indent = true,
-	show_first_indent_level = true,
-	space_char_blankline = " ",
-	show_current_context = true,
-	show_current_context_start = true,
-})
+require("ibl").setup({})
