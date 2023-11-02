2e647e80260da328a90da97268485637366c625a
zanderhavgaard <zander@havgaard.dk>
2023-11-02 15:41:24 +0100

    fix auto-save plugin config, use maintained fork

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -278,9 +278,15 @@ lazy.setup({
 
 	-- automatically save buffer on certain events
 	{
-		"pocco81/auto-save.nvim",
-		conffig = function()
-			require("auto-save").setup({})
+		"okuuva/auto-save.nvim",
+		config = function()
+			require("auto-save").setup({
+				trigger_events = { -- See :h events
+					immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
+					defer_save = {}, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+					cancel_defered_save = {}, -- vim events that cancel a pending deferred save
+				},
+			})
 		end,
 	},
 
