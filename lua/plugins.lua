df3d0a7560a4651a7b395ee9bc245bd038c49d84
zanderhavgaard <zander@havgaard.dk>
2024-10-14 14:55:35 +0200

    update plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -283,7 +283,7 @@ lazy.setup({
 				trigger_events = { -- See :h events
 					immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
 					defer_save = {}, -- vim events that trigger a deferred save (saves after `debounce_delay`)
-					cancel_defered_save = {}, -- vim events that cancel a pending deferred save
+					cancel_deferred_save = {}, -- vim events that cancel a pending deferred save
 				},
 			})
 		end,
